89317465b21a932f8befdfd8fc1a4f1c01d91658
BjarniLeifs <bjarnil10@ru.is>
2016-06-24 14:28:11 +0000

    test

--- test/tests/test.authentication.js
+++ test/tests/test.authentication.js
@@ -1,24 +1,32 @@
-var request = require('supertest');
+var request     = require('supertest');
 var authservice = require('../library/authentication');
+var config      = require('./../../config/configuration');
 
-
-describe('Checking Users API', function () {
-  var server;
-  var userToken;
+describe('Checking Authentication', function () {
+  var server, userToken, user, failuser; 
 
   beforeEach(function () {
     server = require('../../app');
+    server.set('DATABASE_URL', config.testConnectionUrl);
+
     userToken = authservice.getUserToken();
-    
-  });
-  
-  it('Should register user ', function testUserRegister (done) {
-    var user = {
+    user = {
       'username' : 'test',
       'email' : 'test@test.is', 
       'password' : 'test'
+    };  
+    failUse = {
+      'username' : 'test',
+      'email' : 'test@test.is', 
+      'password' : 'tesst'
     };
+  });
 
+  afterEach(function() {
+    server.set('DATABASE_URL', config.connectionUrl);
+  });
+  
+  it('Should register user', function testUserRegister (done) {
     request(server)
       .post('/auth/register')
       .send(user)
@@ -28,8 +36,35 @@ describe('Checking Users API', function () {
           done(err);
         } 
       });
+
       done();
   });
+  
+  it('Should login user', function testUserLogin (done) {
+
+    request(server)
+      .post('/auth/login')
+      .send(user)
+      .expect(200)
+      .end(function (err, ress) {
+        if (err)
+          done(err);
+      });
+      done();
+  });
+
+  it('Should fail login user', function testUserLogin (done) {
+    request(server)
+      .post('/auth/login')
+      .send(failuser)
+      .expect(400)
+      .end(function (err, ress) {
+        if (err)
+          done(err);
+      });
+      done();
+  });
+
 /* // how to use auth on closed calls
   it('Should get all users ', function testGetAllUsers (done) {
     request(server)
